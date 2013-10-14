96d46a9522a98884a4b768e74881047661a22940
Andrew Darqui <andrew.darqui@gmail.com>
2013-10-14 01:16:21 -0400

    parsing auditd messages

--- auditd.js
+++ auditd.js
@@ -5,11 +5,43 @@ var
 var _Auditd = function() {
   var Auditd = this;
 
-  var Auditd.$ = {}
+  Auditd.$ = {}
   var $ = Auditd.$
   $.messages = {}
 
-  Auditd.process = function() {
+  Auditd.process = function(line, cb) {
+    console.log("line:",line);
+    var tokens = line.split(' ')
+
+    var audit = tokens[7];
+    var re = /msg=audit\((.*):(.*)\)/
+    var re_result = re.exec(audit);
+
+    var id = re_result[2];
+
+    if(!$.messages[id]) {
+      var msg = {
+        id : id,
+        host : tokens[3],
+        daemon : tokens[4],
+        node : tokens[5],
+        queue : {},
+      }
+      $.messages[id] = msg;
+    }
+
+    var entry = $.messages[id];
+
+    for(var v in tokens) {
+      var token = tokens[v];
+      var kv = token.split('=',2);
+      console.log(kv);
+    }
+
+    //console.log(msg);
+    console.log(re_result);
+
+
   }
 
   Auditd.init = function() {
--- cat.js
+++ cat.js
@@ -1,5 +1,37 @@
 var
-  lazy = require('lazy'),
-  Auditd = require('./auditd.js');
+  Lazy = require('lazy'),
+  Auditd = require('./auditd.js'),
+  nconf = require('nconf'),
+  fs = require('fs');
 
 
+nconf.argv().env();
+
+var conf = {
+  in : null,
+}
+
+conf.in = nconf.get('in');
+
+var usage = function() {
+  console.log("node cat.js --in=file");
+  process.exit(-1);
+}
+
+if(!conf.in) {
+  usage();
+}
+
+
+var auditd = new Auditd();
+
+
+var lazy = new Lazy(fs.createReadStream(conf.in))
+  .lines
+  .forEach(function(line) {
+    var line = line.toString();
+//    console.log(line)
+    auditd.process(line, function(err,js) {
+      console.log("err",err,"js",js);
+    });
+  })
