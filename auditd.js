dbb943b3d286928bc1934879579806d5b502e6ab
Andrew Darqui <andrew.darqui@gmail.com>
2013-10-14 00:30:00 -0400

    getting started

---
+++ auditd.js
@@ -0,0 +1,21 @@
+var
+  _ = require('underscore');
+
+
+var _Auditd = function() {
+  var Auditd = this;
+
+  var Auditd.$ = {}
+  var $ = Auditd.$
+  $.messages = {}
+
+  Auditd.process = function() {
+  }
+
+  Auditd.init = function() {
+  }
+
+  Auditd.init()
+}
+
+module.exports = _Auditd;
---
+++ cat.js
@@ -0,0 +1,5 @@
+var
+  lazy = require('lazy'),
+  Auditd = require('./auditd.js');
+
+
