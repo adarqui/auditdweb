2693f3a7f85e7f554ad202c08c63655825e3b7a3
Andrew Darqui <andrew.darqui@gmail.com>
2013-10-14 22:26:51 -0400

    adding parsing for normal audit.log ... before this, only remote syslog + audit.log was parsing

--- auditd.js
+++ auditd.js
@@ -10,33 +10,46 @@ var _Auditd = function() {
   $.messages = {}
 
   Auditd.process = function(line, cb) {
-    var tokens, audit, re, re_result, entry, queue_item;
+    var tokens, audit, re, re_result, entry, queue_item, offset;
     tokens = line.split(' ')
-    audit = tokens[7];
 
-    if(tokens.length < 7) return;
+    /* Location of type= */
+    if(tokens[0].indexOf('type=') == 0) {
+      offset = 0
+    }
+    else {
+      offset = 6
+    }
+
+    audit = tokens[offset+1];
+
+//    if(tokens.length < 7) return;
 
     re = /msg=audit\((.*):(.*)\)/
     re_result = re.exec(audit);
-
     if(re_result == null) return;
-
     id = re_result[2];
 
     if(!$.messages[id]) {
       var msg = {
         id : id,
-        host : tokens[3],
-        daemon : tokens[4],
-        node : tokens[5],
         queue : [],
       }
+
+      if(offset > 0) {
+        msg.host = tokens[3]
+        msg.daemon = tokens[4]
+        msg.node = tokens[5]
+      }
+
       $.messages[id] = msg;
     }
 
     entry = $.messages[id];
 
-    tokens.splice(0,5);
+    if(offset > 0) {
+      tokens.splice(0,5);
+    }
 
     queue_item = {};
     _.each(tokens, function(token,index,list) {
